9df30be203445390845ea4962a6d7ef1de714947
Lee <15995683950@163.com>
2026-07-04 00:45:27 +0800

    Add files via upload

---
+++ Notification.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace GrowlNotifiactions
+{
+    public class Notification : INotifyPropertyChanged
+    {
+        private string _message;
+        public string Message
+        {
+            get => _message;
+
+            set
+            {
+                if (_message == value) return;
+                _message = value;
+                OnPropertyChanged("Message");
+            }
+        }
+
+        private int _id;
+        public int Id
+        {
+            get => _id;
+
+            set
+            {
+                if (_id == value) return;
+                _id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        private string _imageUrl;
+        public string ImageUrl
+        {
+            get => _imageUrl;
+
+            set
+            {
+                if (_imageUrl == value) return;
+                _imageUrl = value;
+                OnPropertyChanged("ImageUrl");
+            }
+        }
+
+        private string _title;
+        public string Title
+        {
+            get => _title;
+
+            set
+            {
+                if (_title == value) return;
+                _title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        private string _result;
+
+        public string Result
+        {
+            get => _result;
+            
+            set
+            {
+                if (_result == value) return;
+                _result=value;
+                OnPropertyChanged("Result");
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class Notifications : ObservableCollection<Notification> { }
+}
